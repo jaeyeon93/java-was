180992ca746ccf7782b716d0c80c6052d3a680df
jaeyeon kim <foodsksms@gmail.com>
2018-07-17 16:49:36 +0900

    Post메소드로 User추가하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
+import javax.rmi.CORBA.Util;
+
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -29,25 +31,35 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
-            log.info("starting request");
             log.info("first request info : {}", line);
             String [] arr = line.split(" ");
-            if (arr[1].contains("?")) {
-                Map<String, String> parseResult = HttpRequestUtils.parseQueryString(arr[1].substring(arr[1].indexOf("?")));
-                DataBase.addUser(new User(parseResult.get("userId"), parseResult.get("password"), parseResult.get("name"), parseResult.get("email")));
-            }
+            int contentLength = 0;
             while (!line.equals("")) {
                 line = br.readLine();
-                log.info("request info in while : {}", line);
+                log.info("request info : {}", line);
+                if (line.contains("Content-Length"))
+                    contentLength = getContentLength(line);
             }
+            log.info("url : {}", arr[1]);
+            if (("/user/create").equals(arr[1])) {
+                Map<String, String> paramas = HttpRequestUtils.parseQueryString(IOUtils.readData(br, contentLength));
+                User user = new User(paramas.get("userId"), paramas.get("password"), paramas.get("name"), paramas.get("email"));
+                log.info("user info on RequestHeader : {}", user.toString());
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp/" + arr[1]).toPath());
+            log.info("body : {}", body);
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    public Integer getContentLength(String line) {
+        return Integer.parseInt(line.split(":")[1].trim());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
